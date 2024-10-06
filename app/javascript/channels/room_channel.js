1b826a463e96f0ceef0a317c0a4be63c8ff9866b
kimu805 <kimukimuok805@gmail.com>
2024-10-06 18:44:14 +0900

    Update room_channel.js

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,6 +1,6 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("RoomChannel", {
+window.app = consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -10,10 +10,10 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    alert(data["message"])
   },
 
   speak: function() {
-    return this.perform('speak');
+    return this.perform('speak', {message: message});
   }
 });
