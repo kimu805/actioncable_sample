d4dd179ff19500df091e28a512681e311711da77
kimu805 <kimukimuok805@gmail.com>
2024-10-06 19:08:52 +0900

    Update room_channel.js

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -2,7 +2,7 @@ import consumer from "channels/consumer"
 
 consumer.subscriptions.create("RoomChannel", {
   connected() {
-    document.querySelector("input[data-behavior='room-speaker']").addEventListener("keypress", (event) => {
+    document.querySelector("input[data-behavior='room_speaker']").addEventListener("keypress", (event) => {
       if (event.key === "Enter") {
         this.speak(event.target.value)
         event.target.value = ""
@@ -17,7 +17,7 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
     const element = document.querySelector("#messages")
-    element.insertAdjacentElement("beforeend", data["message"])
+    element.insertAdjacentHTML("beforeend", data["message"])
   },
 
   speak: function(message) {
